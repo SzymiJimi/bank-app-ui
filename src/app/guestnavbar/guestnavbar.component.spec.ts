aa426c27a60574d5a6c70226e243438de579351f
Szymon Dudek <szymek23q@gmail.com>
2018-06-19 01:52:28 +0200

    createoffer component

---
+++ src/app/guestnavbar/guestnavbar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GuestnavbarComponent } from './guestnavbar.component';
+
+describe('GuestnavbarComponent', () => {
+  let component: GuestnavbarComponent;
+  let fixture: ComponentFixture<GuestnavbarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ GuestnavbarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GuestnavbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
