b6abdbff1a037210a25225e8c1474591b1aff690
Szymon Dudek <szymek23q@gmail.com>
2018-06-19 03:58:13 +0200

    working on registry

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Form, FormControl, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-registry',
@@ -10,42 +11,74 @@ export class RegistryComponent implements OnInit {
   registryForm: FormGroup;
   name: FormControl;
   surname: FormControl;
-  login: FormControl;
-  password: FormControl;
-  email: FormControl;
-  pesel: FormControl;
-  phone: FormControl;
+  citizenship: FormControl;
+  mothersName: FormControl;
+  fathersName: FormControl;
 
+  // login: FormControl;
+  // password: FormControl;
+  // email: FormControl;
+  peselNumber: FormControl;
+  telephoneNumber: FormControl;
+  cardNumber: FormControl;
 
 
   createFormControls() {
-
+    this.citizenship = new FormControl('', Validators.required);
+    this.mothersName = new FormControl('', Validators.required);
+    this.fathersName = new FormControl('', Validators.required);
+    this.cardNumber = new FormControl('' , Validators.required);
     this.name = new FormControl('', Validators.required);
-    this.login = new FormControl('', Validators.required);
+    //this.login = new FormControl('', Validators.required);
     this.surname = new FormControl('', Validators.required);
-    this.password = new FormControl('', Validators.required);
-    this.email = new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$')]);
-    this.phone = new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]);
-    this.pesel = new FormControl('', [Validators.required,  Validators.pattern('^[0-9]{11}')] );
+   // this.password = new FormControl('', Validators.required);
+   // this.email = new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$')]);
+    this.telephoneNumber = new FormControl('', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]);
+    this.peselNumber = new FormControl('', [Validators.required,  Validators.pattern('^[0-9]{11}')] );
   }
 
   createForm() {
     this.registryForm = new FormGroup({
       name: this.name,
       surname: this.surname,
-      login: this.login,
-      password: this.password,
-      email: this.email,
-      pesel: this.pesel,
-      phone: this.phone
+      citizenship: this.citizenship,
+      mothersName: this.mothersName,
+      fathersName: this.fathersName,
+      //idCardNumber: this.cardNumber,
+      // login: this.login,
+      // password: this.password,
+      // email: this.email,
+      peselNumber: this.peselNumber,
+      telephoneNumber: this.telephoneNumber
     });
   }
 
-  constructor() { }
+  generateCardId() {
+    this.cardNumber = Math.floor((Math.random() * 999999) + 100000);
+    (<HTMLInputElement>document.getElementById('idCardNumber')).innerHTML = 'NSK'+  this.cardNumber;
+  }
+
+  send() {
+    const data = JSON.stringify(this.registryForm.value);
+    const data2 = JSON.parse(data);
+    console.log(data);
+    console.log(data2);
+    this.http.post('http://localhost:8080/person/new', JSON.parse(data) ,
+      {
+        headers: { 'Content-Type': 'application/json' },
+        responseType: 'json'
+      })
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
+
 
   ngOnInit() {
     this.createFormControls();
     this.createForm();
   }
+  constructor( private http: HttpClient) {
 
+  }
 }
